621b2dac7db8c7cae2d9c1c30295d5e627b199fb
jhon faccin <jhonfaccin@gmail.com>
2018-09-18 11:13:15 -0300

    fatoracao do codigo

--- js/fichaCrtl.js
+++ js/fichaCrtl.js
@@ -7,7 +7,6 @@ angular.module("ficha").controller('fichaCrtl',
             $scope.buscar = function (pesquisa) {
                 fichaService.pesquisar(pesquisa).then(function(response){
                     $scope.fichas = response.data || [];
-                    console.log($scope.fichas)
                 });
                 $scope.mostrarFichasCadastradas = true;
             }
